10fb86ad628689eb3d609d2ec8756836a26abc2c
RSSchermer <roland0507@gmail.com>
2018-08-10 17:32:32 +0200

    GPU command infrastructure

--- src/core/gpu_command.rs
+++ src/core/gpu_command.rs
@@ -27,16 +27,46 @@ impl<T, Ec> From<Box<T>> for CommandObject<T, Ec> where T: GpuCommand<Ec> {
     }
 }
 
-pub trait Execute<A, Ec> where A: GpuCommand<Ec> {
+pub trait GpuCommandExt<A, Ec> where A: GpuCommand<Ec> {
     fn execute(self, execution_context: &mut Ec) -> Result<A::Output, A::Error>;
+
+    fn map<B, F>(self, f: F) -> Map<A, F, Ec> where F: FnOnce(A::Output) -> B;
+
+    fn map_err<B, F>(self, f: F) -> MapErr<A, F, Ec> where F: FnOnce(A::Error) -> B;
+
+    fn then<B, F>(self, f: F) -> Then<A, F, Ec> where B: GpuCommand<Ec>, F: FnOnce(Result<A::Output, A::Error>) -> B;
+
+    fn and_then<B, F>(self, f: F) -> AndThen<A, F, Ec> where B: GpuCommand<Ec, Error=A::Error>, F: FnOnce(A::Output) -> B;
+
+    fn or_else<B, F>(self, f: F) -> OrElse<A, F, Ec> where B: GpuCommand<Ec, Output=A::Output>, F: FnOnce(A::Error) -> B;
 }
 
-impl<T, A, Ec> Execute<A, Ec> for T where T: Into<CommandObject<A, Ec>>, A: GpuCommand<Ec> {
+impl<T, A, Ec> GpuCommandExt<A, Ec> for T where T: Into<CommandObject<A, Ec>>, A: GpuCommand<Ec> {
     fn execute(self, execution_context: &mut Ec) -> Result<A::Output, A::Error> {
         match self.into() {
             CommandObject::Static(executable, _) => executable.execute_static(execution_context),
             CommandObject::Dynamic(boxed_executable) => boxed_executable.execute_dynamic(execution_context)
         }
+    }
+
+    fn map<B, F>(self, f: F) -> Map<A, F, Ec> where F: FnOnce(A::Output) -> B {
+        Map::new(self, f)
+    }
+
+    fn map_err<B, F>(self, f: F) -> MapErr<A, F, Ec> where F: FnOnce(A::Error) -> B {
+        MapErr::new(self, f)
+    }
+
+    fn then<B, F>(self, f: F) -> Then<A, F, Ec> where B: GpuCommand<Ec>, F: FnOnce(Result<A::Output, A::Error>) -> B {
+        Then::new(self, f)
+    }
+
+    fn and_then<B, F>(self, f: F) -> AndThen<A, F, Ec> where B: GpuCommand<Ec, Error=A::Error>, F: FnOnce(A::Output) -> B {
+        AndThen::new(self, f)
+    }
+
+    fn or_else<B, F>(self, f: F) -> OrElse<A, F, Ec> where B: GpuCommand<Ec, Output=A::Output>, F: FnOnce(A::Error) -> B {
+        OrElse::new(self, f)
     }
 }
 
@@ -262,44 +292,173 @@ mod tests {
 
     struct TestContext;
 
-    struct Return1;
+    #[derive(Debug)]
+    struct ReturnOk<T>(T);
 
-    impl GpuCommand<TestContext> for Return1 {
-        type Output = i32;
+    impl<T> GpuCommand<TestContext> for ReturnOk<T> {
+        type Output = T;
 
         type Error = ();
 
         fn execute_static(self, _execution_context: &mut TestContext) -> Result<Self::Output, Self::Error> {
-            Ok(1)
+            Ok(self.0)
         }
 
         fn execute_dynamic(self: Box<Self>, _execution_context: &mut TestContext) -> Result<Self::Output, Self::Error> {
-            Ok(1)
+            Ok(self.0)
+        }
+    }
+
+    #[derive(Debug)]
+    struct ReturnErr<T>(T);
+
+    impl<T> GpuCommand<TestContext> for ReturnErr<T> {
+        type Output = ();
+
+        type Error = T;
+
+        fn execute_static(self, _execution_context: &mut TestContext) -> Result<Self::Output, Self::Error> {
+            Err(self.0)
+        }
+
+        fn execute_dynamic(self: Box<Self>, _execution_context: &mut TestContext) -> Result<Self::Output, Self::Error> {
+            Err(self.0)
         }
     }
 
     #[test]
-    fn test_map_execute() {
+    fn test_map_execute_static() {
         let mut context = TestContext;
-        let map = Map::new(Return1, |v| v * 2);
+        let map = Map::new(ReturnOk(1), |v| v * 2);
 
         assert_eq!(map.execute(&mut context), Ok(2));
     }
 
     #[test]
-    fn test_map_execute_boxed() {
+    fn test_map_execute_dynamic() {
         let mut context = TestContext;
-        let map = Map::new(Return1, |v| v * 2);
+        let map = Map::new(ReturnOk(1), |v| v * 2);
         let boxed_map = Box::new(map);
 
         assert_eq!(boxed_map.execute(&mut context), Ok(2));
     }
 
     #[test]
-    fn test_map_with_boxed_command() {
+    fn test_map_err_execute_static() {
         let mut context = TestContext;
-        let map = Map::new(Box::new(Return1), |v| v * 2);
+        let map_err = MapErr::new(ReturnErr(1), |v| v * 2);
 
-        assert_eq!(map.execute(&mut context), Ok(2));
+        assert_eq!(map_err.execute(&mut context), Err(2));
+    }
+
+    #[test]
+    fn test_map_err_execute_dynamic() {
+        let mut context = TestContext;
+        let map = MapErr::new(ReturnErr(1), |v| v * 2);
+        let boxed_map_err = Box::new(map);
+
+        assert_eq!(boxed_map_err.execute(&mut context), Err(2));
+    }
+
+    #[test]
+    fn test_ok_then_execute_static() {
+        let mut context = TestContext;
+        let then = Then::new(ReturnOk(1), |res| ReturnOk(res.unwrap() * 2));
+
+        assert_eq!(then.execute(&mut context), Ok(2));
+    }
+
+    #[test]
+    fn test_ok_then_execute_dynamic() {
+        let mut context = TestContext;
+        let then = Then::new(ReturnOk(1), |res| ReturnOk(res.unwrap() * 2));
+        let boxed_then = Box::new(then);
+
+        assert_eq!(boxed_then.execute(&mut context), Ok(2));
+    }
+
+    #[test]
+    fn test_err_then_execute_static() {
+        let mut context = TestContext;
+        let then = Then::new(ReturnErr(1), |res| ReturnErr(res.unwrap_err() * 2));
+
+        assert_eq!(then.execute(&mut context), Err(2));
+    }
+
+    #[test]
+    fn test_err_then_execute_dynamic() {
+        let mut context = TestContext;
+        let then = Then::new(ReturnErr(1), |res| ReturnErr(res.unwrap_err() * 2));
+        let boxed_then = Box::new(then);
+
+        assert_eq!(boxed_then.execute(&mut context), Err(2));
+    }
+
+    #[test]
+    fn test_ok_and_then_execute_static() {
+        let mut context = TestContext;
+        let and_then = AndThen::new(ReturnOk(1), |v| ReturnOk(v * 2));
+
+        assert_eq!(and_then.execute(&mut context), Ok(2));
+    }
+
+    #[test]
+    fn test_ok_and_then_execute_dynamic() {
+        let mut context = TestContext;
+        let and_then = AndThen::new(ReturnOk(1), |v| ReturnOk(v * 2));
+        let boxed_and_then = Box::new(and_then);
+
+        assert_eq!(boxed_and_then.execute(&mut context), Ok(2));
+    }
+
+    #[test]
+    fn test_err_and_then_execute_static() {
+        let mut context = TestContext;
+        let and_then = AndThen::new(ReturnErr(1), |_| ReturnErr(2));
+
+        assert_eq!(and_then.execute(&mut context), Err(1));
+    }
+
+    #[test]
+    fn test_err_and_then_execute_dynamic() {
+        let mut context = TestContext;
+        let and_then = AndThen::new(ReturnErr(1), |_| ReturnErr(2));
+        let boxed_and_then = Box::new(and_then);
+
+        assert_eq!(boxed_and_then.execute(&mut context), Err(1));
+    }
+
+    #[test]
+    fn test_ok_or_else_execute_static() {
+        let mut context = TestContext;
+        let or_else = OrElse::new(ReturnOk(1), |_| ReturnOk(2));
+
+        assert_eq!(or_else.execute(&mut context), Ok(1));
+    }
+
+    #[test]
+    fn test_ok_or_else_execute_dynamic() {
+        let mut context = TestContext;
+        let or_else = OrElse::new(ReturnOk(1), |_| ReturnOk(2));
+        let boxed_or_else = Box::new(or_else);
+
+        assert_eq!(boxed_or_else.execute(&mut context), Ok(1));
+    }
+
+    #[test]
+    fn test_err_or_else_execute_static() {
+        let mut context = TestContext;
+        let or_else = OrElse::new(ReturnErr(1), |v| ReturnErr(v * 2));
+
+        assert_eq!(or_else.execute(&mut context), Err(2));
+    }
+
+    #[test]
+    fn test_err_or_else_execute_dynamic() {
+        let mut context = TestContext;
+        let or_else = OrElse::new(ReturnErr(1), |v| ReturnErr(v * 2));
+        let boxed_or_else = Box::new(or_else);
+
+        assert_eq!(boxed_or_else.execute(&mut context), Err(2));
     }
 }
