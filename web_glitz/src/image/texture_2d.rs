be03e3dbdc318e69475339e3cf52e885893614fe
RSSchermer <roland0507@gmail.com>
2019-01-31 23:25:06 +0100

    Implement mut texture views with deref

--- web_glitz/src/image/texture_2d.rs
+++ web_glitz/src/image/texture_2d.rs
@@ -18,6 +18,7 @@ use crate::runtime::dynamic_state::ContextUpdate;
 use crate::runtime::{Connection, RenderingContext, ContextMismatch};
 use crate::task::{GpuTask, Progress};
 use crate::util::{arc_get_mut_unchecked, identical, JsId};
+use std::ops::Deref;
 
 
 pub struct Texture2D<F> {
@@ -346,66 +347,40 @@ where
 }
 
 pub struct Texture2DLevelsMut<'a, F> {
-    handle: &'a mut Texture2D<F>
+    inner: Texture2DLevels<'a, F>
 }
 
 impl<'a, F> Texture2DLevelsMut<'a, F>
     where
         F: TextureFormat,
 {
-    pub fn len(&self) -> usize {
-        self.handle.data.levels
-    }
-
-    pub fn get(&self, level: usize) -> Option<Texture2DLevel<F>> {
-        if level < self.handle.data.levels {
-            Some(Texture2DLevel {
-                handle: &self.handle,
-                level,
-            })
-        } else {
-            None
-        }
-    }
-
     pub fn get_mut(&mut self, level: usize) -> Option<Texture2DLevelMut<F>> {
         if level < self.handle.data.levels {
             Some(Texture2DLevelMut {
-                handle: &mut self.handle,
-                level,
+                inner: Texture2DLevel {
+                    handle: &self.inner.handle,
+                    level
+                },
             })
         } else {
             None
-        }
-    }
-
-    pub unsafe fn get_unchecked(&self, level: usize) -> Texture2DLevel<F> {
-        Texture2DLevel {
-            handle: &self.handle,
-            level,
         }
     }
 
     pub unsafe fn get_mut_unchecked(&mut self, level: usize) -> Texture2DLevelMut<F> {
         Texture2DLevelMut {
-            handle: &mut self.handle,
-            level,
-        }
-    }
-
-    pub fn iter(&self) -> Texture2DLevelsIter<F> {
-        Texture2DLevelsIter {
-            handle: &self.handle,
-            current_level: 0,
-            end_level: self.handle.data.levels,
+            inner: Texture2DLevel {
+                handle: &self.inner.handle,
+                level
+            },
         }
     }
 
     pub fn iter_mut(&mut self) -> Texture2DLevelsMutIter<F> {
         Texture2DLevelsMutIter {
             current_level: 0,
-            end_level: self.handle.data.levels,
-            handle: &mut self.handle,
+            end_level: self.inner.handle.data.levels,
+            handle: &self.inner.handle,
         }
     }
 }
@@ -421,14 +396,23 @@ impl<'a, F> IntoIterator for Texture2DLevelsMut<'a, F>
     fn into_iter(self) -> Self::IntoIter {
         Texture2DLevelsMutIter {
             current_level: 0,
-            end_level: self.handle.data.levels,
-            handle: self.handle,
+            end_level: self.inner.handle.data.levels,
+            handle: &self.inner.handle,
         }
     }
 }
 
+impl<'a, F> Deref for Texture2DLevelsMut<'a, F> where
+    F: TextureFormat, {
+    type Target = Texture2DLevels<'a, F>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
 pub struct Texture2DLevelsMutIter<'a, F> {
-    handle: &'a mut Texture2D<F>,
+    handle: &'a Texture2D<F>,
     current_level: usize,
     end_level: usize,
 }
@@ -445,14 +429,11 @@ impl<'a, F> Iterator for Texture2DLevelsMutIter<'a, F>
         if level < self.end_level {
             self.current_level += 1;
 
-            // Extending the lifetime here is safe, because the implementation guarantees that it
-            // will never hand out a reference to the same level twice, and having concurrent mut
-            // references to different levels is safe and arguably desirable (e.g. it is perfectly
-            // fine to attach 2 different levels of a texture to a framebuffer).
-
             Some(Texture2DLevelMut {
-                handle: unsafe { mem::transmute(&mut self.handle) },
-                level,
+                inner: Texture2DLevel {
+                    handle: &self.handle,
+                    level
+                }
             })
         } else {
             None
@@ -461,45 +442,14 @@ impl<'a, F> Iterator for Texture2DLevelsMutIter<'a, F>
 }
 
 pub struct Texture2DLevelMut<'a, F> {
-    handle: &'a mut Texture2D<F>,
-    level: usize,
+    inner: Texture2DLevel<'a, F>
 }
 
-impl<'a, F> Texture2DLevelMut<'a, F>
-    where
-        F: TextureFormat,
-{
-    pub fn level(&self) -> usize {
-        self.level
-    }
+impl<'a, F> Deref for Texture2DLevelMut<'a, F> {
+    type Target = Texture2DLevel<'a, F>;
 
-    pub fn width(&self) -> u32 {
-        mipmap_size(self.handle.data.width, self.level)
-    }
-
-    pub fn height(&self) -> u32 {
-        mipmap_size(self.handle.data.height, self.level)
-    }
-
-    pub fn sub_image(&self, region: Region2D) -> Texture2DLevelSubImage<F> {
-        Texture2DLevelSubImage {
-            handle: &self.handle,
-            level: self.level,
-            region,
-        }
-    }
-
-    pub fn upload_task<D, T>(&self, data: Image2DSource<D, T>) -> Texture2DUploadTask<D, T, F>
-        where
-            T: ClientFormat<F>,
-    {
-        Texture2DUploadTask {
-            data,
-            texture_data: self.handle.data.clone(),
-            level: self.level,
-            region: Region2D::Fill,
-            _marker: marker::PhantomData,
-        }
+    fn deref(&self) -> &Self::Target {
+        &self.inner
     }
 }
 
